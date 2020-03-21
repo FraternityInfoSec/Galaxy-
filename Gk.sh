d7f0993e20cdc520be97892a505f32cd90beab5e
ProfessorCipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-03-21 12:53:13 +0530

    Update Gk.sh

--- Gk.sh
+++ Gk.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-command -v curl > /dev/null 2>&1 || {  echo >&2 "I require curl but it is not installed. \n installing it.."; apt install curl -y; }
+command -v curl > /dev/null 2>&1 || {  echo >&2 "we require curl but it is not installed. \n installing it.."; apt install curl -y; }
 
-command -v python > /dev/null 2>&1 || {  echo >&2 "I require python but it is not installed. \n installing it.."; apt install python -y; }
+command -v python > /dev/null 2>&1 || {  echo >&2 "we require python but it is not installed. \n installing it.."; apt install python -y; }
 
-command -v pip > /dev/null 2>&1 || {  echo >&2 "I require pip but it is not installed. \n installing it.."; curl https:bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py ; }
+command -v pip > /dev/null 2>&1 || {  echo >&2 "we require pip but it is not installed. \n installing it.."; curl https:bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py ; }
 
-command -v toilet > /dev/null 2>&1 || {  echo >&2 "I require toilet package but it is not installed. \n installing it.."; apt install toilet -y; }
+command -v toilet > /dev/null 2>&1 || {  echo >&2 "we require toilet package but it is not installed. \n installing it.."; apt install toilet -y; }
 
+command -v fish > /dev/ull 2>&1 || { echo >&2 "we require fish but it isn't important and not installed it is required in case if you forget coding for installation later. \installing it.."apt install fish -y;} 
