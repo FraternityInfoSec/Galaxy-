4fbdadb5462ab71e0d6f5d1f68c479dafd19bb50
ProfessorCipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-03-22 11:17:25 +0530

    Update msg.py

--- msg.py
+++ msg.py
@@ -1,10 +1,10 @@
 import requests, os, sys, time
 
-print("\033[96mCoded by https://github.com/ProfessorCipher\033[0m")
+print("\033[98mCoded by https://github.com/ProfessorCipher\033[0m")
 
-print("\033[93m Version 2.0 \033[0m")
+print("\033[96m Version 2.0 \033[0m")
 
-print("\033[91m Checking dependencies... \033[0m")
+print("\033[94m checking server... \033[0m")
 
 time.sleep(2)
 
@@ -12,9 +12,9 @@ os.system("bash rq.sh")
 
 def menu() :
 
-    print("\033[91m1.\033[0m \033[92mSend anonymous sms\033[0m")
+    print("\033[96m1.\033[0m \033[92mSend anonymous sms\033[0m")
 
-    print("\033[91m2.\033[0m \033[92mCheck status of sms\033[0m")
+    print("\033[96m2.\033[0m \033[92mCheck status of sms\033[0m")
 
 def control() :
 
@@ -34,9 +34,9 @@ def control() :
 
 def sms() :
 
-   phone_no = input("enter phone number : ")
+   phone_no = input("\033[96menter phone number : \033[0m")
 
-   msg = input("message to send : ")
+   msg = input("\033[96message to send : \033[0m")
 
    resp = requests.post('https://textbelt.com/text',{
 
@@ -66,7 +66,7 @@ os.system("clear")
 
 os.system("toilet -f mono12 -F gay GALAXY- ")
 
-print("\033[96mCoded by github.com/ProfessorCipher")
+print("\033[98mCoded by github.com/ProfessorCipher")
 
 print("Re-updated tool by 445H4N or also known as PROF.CIPHER :)")
 
